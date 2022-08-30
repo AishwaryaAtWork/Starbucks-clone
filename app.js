7ddfa67c41fc0b6948928243b1863a99dce4366b
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2022-08-31 01:08:38 +0530

    A bit functionality added

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-console.log('start');
-
 function imgSlider(img){
     document.querySelector('.starbucks').src='src/'+img+'.png';
+}
+
+function changeCircleColor(color){
+    const circle=document.querySelector('.circle');
+    circle.style.background=color;
 }
